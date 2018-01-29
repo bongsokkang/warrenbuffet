07f306a56008c72e76b30f9ec760d8c109372357
Bongsok Kang <bongsokkang@gmail.com>
2018-01-29 02:29:37 -0800

    helloworld

---
+++ models/Article3.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+
+var ArticleSchema = new Schema({
+
+  title: {
+    type: String,
+    required: true
+  },
+
+  link: {
+    type: String,
+    required: true
+  },
+
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+var Article3 = mongoose.model("Article3", ArticleSchema);
+
+
+module.exports = Article3;
